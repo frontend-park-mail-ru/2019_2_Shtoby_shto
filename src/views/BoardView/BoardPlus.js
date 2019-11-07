6bddfedf72f0cbe17f6210758c3882a24e03a4c2
aaa <aleshka2012@gmail.com>
2019-11-08 02:55:43 +0300

    промежуточный коммит

--- src/views/BoardView/BoardPlus.js
+++ src/views/BoardView/BoardPlus.js
@@ -1,11 +1,35 @@
-import Component from '../../modules/Component';
+// import Component from '../../modules/Component';
+import StateComponent from '../../components/StateComponent';
 
-export default class BoardPlus extends Component {
-  constructor() {
-    super({classes: ['board__plus', 'board__tab']});
+class Input extends Component {
+  constructor(ownProps) {
+    super({tag: 'input'}, ...ownProps);
+
+    this.element
   }
 
-  generateContent() {
-    return '+';
+  setOnBlur(fun) {
+    this.onblur = fun;
+  }
+
+  executeOnBlur(text) {
+    this.onblur(text);
+  }
+}
+
+class TransformingInput extends StateComponent {
+  constructor(otherState) {
+    super();
+
+    // this.addStates({
+      // input: new Component
+    // })
+  }
+}
+
+export default class BoardPlus extends StateComponent {
+  constructor() {
+    super();
+
   }
 }
