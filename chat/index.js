b827e22f9bd3771d4819a275ce97ad048c1d4e21
Alexey Molchanov <48250291+Alexosis@users.noreply.github.com>
2019-11-23 15:14:47 +0300

    time

--- chat/index.js
+++ chat/index.js
@@ -2,16 +2,29 @@ import socket from './WS.js';
 
 socket.wsSend('getChats');
 socket.onmessage = function(event) {
+  const options ={
+    hour: 'numeric',
+    minute: 'numeric',
+  };
   const inputField = document.getElementsByClassName('chat-frame__input')[0];
   const msg = document.createElement('div');
-  msg.innerHTML = `<div class="container answer">
-    <img src="" alt="Avatar" class="chat-frame__answer-avatar">
-    <p>eeeeeeeeeeee</p>
-    <span class="chat-frame__answer-time">11:05</span>
+  msg.innerHTML = `<div class="container">
+    <img src="" alt="Avatar">
+    <p>${event.data}</p>
+    <span class="chat-frame__asking-time">
+    ${new Date().toLocaleString(options)}</span>
 </div>`;
   inputField.before(msg);
 };
 
 export function sendMessage() {
-
+  const message = document.getElementsByClassName('chat-frame__input')[0];
+  socket.send(message.value);
+  const msg = document.createElement('div');
+  msg.innerHTML = `<div class="container answer">
+    <img src="" alt="Avatar" class="chat-frame__answer-avatar">
+    <p>${message.value}</p>
+    <span class="chat-frame__answer-time">11:05</span>
+</div>`;
+  message.before(msg);
 }
