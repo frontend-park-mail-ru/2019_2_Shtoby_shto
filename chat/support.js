1301d151848d839663a6409c6131e8faabf644c0
yuliya <yuliya.sm7@gmail.com>
2019-11-23 13:47:03 +0300

    getChats

--- chat/support.js
+++ chat/support.js
@@ -1,7 +1,19 @@
 import socket from '/WS.js';
 
 socket.wsSend("getChats");
+
 socket.onmessage = function(event) {
-  const msg = document.getElementsByTagName('td')[1];
-  msg.innerText = event.data;
+  const Chats_count = 5;
+  const name = "Саня";
+  const table = document.getElementsByTagName('tbody')[0];
+  for (let i = 0; i < Chats_count; i++) {
+    const row = document.createElement('tr');
+    row.innerHTML = `<th scope="row">` + (i+1) + `</th>
+                    <td>` + name + `</td>
+                    <td>` + event.data +`</td>`;
+    row.onclick = function() {
+      window.location.href = 'index.html'
+    };
+    table.appendChild(row);
+  }
 };
