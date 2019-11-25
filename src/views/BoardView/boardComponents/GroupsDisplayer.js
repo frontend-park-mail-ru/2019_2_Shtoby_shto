608161b75d820da92091a124819dcddc45532d83
aaa <aleshka2012@gmail.com>
2019-11-25 10:42:23 +0300

    пофиксил двойное добавление групп

--- src/views/BoardView/boardComponents/GroupsDisplayer.js
+++ src/views/BoardView/boardComponents/GroupsDisplayer.js
@@ -25,8 +25,10 @@ export default class GroupDisplayer extends Component {
     this.addChild(new Component({classes: ['group__container']})
         .addChild(new GroupPlus().setOnBlur((text) => {
           if (text) dispatch(group.createGroup(text));
-        }).setOnChange((text) => {
-          if (text) dispatch(group.createGroup(text));
-        })));
+        })
+        // .setOnChange((text) => {
+        //   if (text) dispatch(group.createGroup(text));
+        ));
+    // })));
   }
 }
