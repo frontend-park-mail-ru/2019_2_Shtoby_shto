5866823ab2e7543a4008f5be1249409dadbdaaed
yuliya <yuliya.sm7@gmail.com>
2019-12-10 22:40:42 +0300

    сообщение об удалении (не работает)

--- src/views/BoardView/boardComponents/Trashbin.js
+++ src/views/BoardView/boardComponents/Trashbin.js
@@ -1,17 +1,33 @@
 import DNDComponent from '../../../modules/DNDComponent';
+import Component from '../../../modules/Component';
 
 const img = require('./trash.png');
 
 export default class Trashbin extends DNDComponent {
   constructor() {
     super({
+      tag: 'div'
+    });
+
+    this.msg = new Component({
+      tag: 'h4',
+      classes: ['trash', 'trash__msg'],
+      attrs: {hidden: true},
+      content: "Скройся уже"
+    });
+
+    this.addChild(this.msg);
+
+    this.addChild(new Component({
       tag: 'img',
       attrs: {src: `${img}`},
       classes: ['trash'],
-    });
+    }));
 
     this.makeDroppable((_, placed) => {
-      if ('del' in placed) placed.del();
+      if ('del' in placed) {
+        placed.del();
+      }
     });
   }
 }
